9929c7a9fc54e77cd694c93d4e704e11b1752151
jmsaylor <johnmsaylor@gmail.com>
2022-05-14 16:01:03 -0400

    initial grid and print loop

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -1,5 +1,11 @@
-(ns scratch.core
-  (:require [scratch.library :as lib]))
+(ns scratch.core)
 
+(defn tester [x] x)
 
+(def initial (vec (repeat 3 (vec (repeat 3 0)))))
 
+(defn print-square [square]
+  (loop [n 0]
+    (when (< n 3)
+      (println (nth square n))
+      (recur (inc n)))))
