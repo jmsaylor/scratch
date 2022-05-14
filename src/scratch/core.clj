8e1c342cc5459ab94af69d278ede22802362d634
jmsaylor <johnmsaylor@gmail.com>
2022-05-14 15:14:10 -0400

    refactor

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -1,13 +1,5 @@
 (ns scratch.core
   (:require [scratch.library :as lib]))
 
-(def zabo lib/rabo)
-(def zayo lib/yabo)
 
-(defn yapo [[x y]]
-  (println x y)
-  (Thread/sleep 100)
-  (if (< x 2000)
-  (recur [(zabo x) (zayo y)])))
 
-(defn rapo [x] (yapo [x x]))
--- src/scratch/library.clj
+++ src/scratch/library.clj
@@ -3,3 +3,11 @@
 (defn rabo [x] (+ x 2))
 
 (defn yabo [x] (+ x 3))
+
+(defn yapo [[x y]]
+    (println x y)
+    (Thread/sleep 100)
+    (if (< x 2000)
+    (recur [(zabo x) (zayo y)])))
+  
+  (defn rapo [x] (yapo [x x]))
