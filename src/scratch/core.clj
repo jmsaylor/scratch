212e1ee98127ae9f67678530b8786617362eb544
jmsaylor <johnmsaylor@gmail.com>
2022-05-15 10:32:03 -0400

    first generation

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -1,47 +1,13 @@
-(ns scratch.core)
+(ns scratch.core
+  (:require [scratch.library :as d]))
 
-(defn tester [x] x)
+(def initial-grid d/initial-grid)
 
-(def initial-grid (vec (repeat 3 (vec (repeat 3 0)))))
+(defn tester [] (d/get-random-direction))
 
-(defn swap-it [x] (if (= x 0) 1 0))
-
-(defn swap-row [row] (let [[a b c] row] [(swap-it a) (swap-it b) (swap-it c)]))
-
-(defn draw [direction grid]
-  (let [[a b c] grid]
-    (case direction
-      :h-top [(swap-row a) b c]
-      :h-middle [a (swap-row b) c]
-      :h-bottom [a b (swap-row c)])))
-
-(defn draw-horizontal-top [grid]
-  (let [[a b c] grid]
-    [(swap-row a) b c]))
-
-(defn draw-horizontal-middle [grid]
-  (let [[a b c] grid]
-    [a (swap-row b) c]))
-
-;; (defn draw-horizontal-middle [grid]
-;;   (let [[[a b c] [d e f] [g h i]] grid]
-;;     [[a b c]
-;;      [(swap-it d) (swap-it e) (swap-it f)]
-;;      [g h i]]))
-
-;;      (defn draw-horizontal-middle [grid]
-;;       (let [[[a b c] [d e f] [g h i]] grid]
-;;         [[a b c]
-;;          [(swap-it d) (swap-it e) (swap-it f)]
-;;          [g h i]]))
-;; (defn draw-top [grid]
-;;   (vec
-;;    ((vec ((swap-it (get grid 0 0)) (swap-it (get grid 0 1)) (swap-it (get grid 0 2))))
-;;     (vec (get grid 1))
-;;     (vec (get grid 2)))))
-
-(defn print-square [square]
-  (loop [n 0]
-    (when (< n 3)
-      (println (nth square n))
-      (recur (inc n)))))
+(defn main []
+  (loop [n 0 grid d/initial-grid]
+    (when (< n 2000)
+      (Thread/sleep 1000)
+      (d/print-grid grid)
+      (recur (inc n) (d/draw (d/get-random-direction) grid)))))
--- src/scratch/library.clj
+++ src/scratch/library.clj
@@ -1,13 +1,47 @@
 (ns scratch.library)
 
-(defn rabo [x] (+ x 2))
+(defn swap-it [x] (if (= x 0) 1 0))
 
-(defn yabo [x] (+ x 3))
+(def initial-grid (vec (repeat 3 (vec (repeat 3 0)))))
 
-(defn yapo [[x y]]
-    (println x y)
-    (Thread/sleep 100)
-    (if (< x 2000)
-    (recur [(zabo x) (zayo y)])))
-  
-  (defn rapo [x] (yapo [x x]))
+(defn get-random-direction [] (rand-nth [:d-up :v-right :v-middle :v-left :d-down :h-bottom :h-middle :h-top]))
+
+(defn draw [direction grid]
+  (let [[a b c] grid]
+    (case direction
+      :h-top [(swap-row a) b c]
+      :h-middle [a (swap-row b) c]
+      :h-bottom [a b (swap-row c)]
+      :v-left (draw-vertical-left grid)
+      :v-middle (draw-vertical-middle grid)
+      :v-right (draw-vertical-right grid)
+      :d-up (draw-diagonal-up grid)
+      :d-down (draw-diagonal-down grid))))
+
+(defn swap-row [row] (let [[a b c] row] [(swap-it a) (swap-it b) (swap-it c)]))
+
+(defn draw-diagonal-up [grid]
+  (let [[[a b c] [d e f] [g h i]] grid]
+    [[a b (swap-it c)] [d (swap-it e) f] [(swap-it g) h i]]))
+
+(defn draw-diagonal-down [grid]
+  (let [[[a b c] [d e f] [g h i]] grid]
+    [[(swap-it a) b c] [d (swap-it e) f] [g h (swap-it i)]]))
+
+(defn draw-vertical-left [grid]
+  (let [[[a b c] [d e f] [g h i]] grid]
+    [[(swap-it a) b c] [(swap-it d) e f] [(swap-it g) h i]]))
+
+(defn draw-vertical-middle [grid]
+  (let [[[a b c] [d e f] [g h i]] grid]
+    [[a (swap-it b) c] [d (swap-it e) f] [g (swap-it h) i]]))
+
+(defn draw-vertical-right [grid]
+  (let [[[a b c] [d e f] [g h i]] grid]
+    [[a b (swap-it c)] [d e (swap-it f)] [g h (swap-it i)]]))
+
+(defn print-grid [grid]
+  (loop [n 0]
+    (when (< n 3)
+      (println (nth grid n))
+      (recur (inc n)))))
